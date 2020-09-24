16c800854ad486481fed421e2eaf13886b6b1726
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-24 21:55:56 +0700

    Add project files.

---
+++ Practical4Ex2/IStudentMark.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practical4Ex2
+{
+    interface IStudentMark
+    {
+        public string Fullname { set; get; }
+        public int Id { set; get; }
+        public string Class { set; get; }
+        public int Semester { set; get; }
+        public float Averagemark { set; get; }
+
+        public void Display() { }
+    }
+}
